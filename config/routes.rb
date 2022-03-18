3054145b69f876b33b3a2f7c532a6cf67d084f33
David Garcia <davidjosegarcia12@gmail.com>
2022-03-18 17:21:20 -0500

    Primer commit
    para montar el proyecto a github

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do 
+  root to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :home, only:[:index]
+  resources :areas
+ 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
